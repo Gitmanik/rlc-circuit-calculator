2d49de2c2ac6e65c4fad3beaa7a286f6e47498f4
Pawel Reich <thegitman@outlook.com>
2024-03-18 18:32:55 +0100

    Cleaned up setup_charts function
    Created stub calculate function to fill up charts

--- main.js
+++ main.js
@@ -6,17 +6,7 @@ window.addEventListener('load', async function()
 {
     setup_charts();
     load_default_values();
-
-
-
-
-    myChart = new Chart(document.getElementById('bode'), bode_chart_config);
-    data.forEach(n => {
-        myChart.data.labels.push(n + "");
-        myChart.data.datasets[0].data.push(n.year);
-        myChart.data.datasets[1].data.push(n.count);
-    });
-    myChart.update();
+    calculate();
 });
 
 function load_default_values()
@@ -26,7 +16,14 @@ function load_default_values()
 
 function setup_charts()
 {
-    // FIXME: usunac
+    input_function_chart = new Chart(document.getElementById('input_function'), input_function_config);
+    output_function_chart = new Chart(document.getElementById('output_function'), output_function_config);
+    bode_chart = new Chart(document.getElementById('bode'), bode_chart_config);
+}
+
+// FIXME: Stub function
+function calculate()
+{
     const data = [
         { year: 2010, count: 10 },
         { year: 2011, count: 20 },
@@ -36,12 +33,19 @@ function setup_charts()
         { year: 2015, count: 30 },
         { year: 2016, count: 28 },
       ];
-    
 
-    input_function_chart = new Chart(document.getElementById('input_function'), input_function_config);
-    input_function_chart.data.labels.push(data.map(row => row.year));
-    input_function_chart.data.datasets.label = 'label';
-    input_function_chart.data.datasets.data = data.map(row => row.count)
+    input_function_chart.data.labels = data.map(row => row.year);
+    input_function_chart.data.datasets[0].data = data.map(row => row.count);
+    input_function_chart.update();
 
-    output_function_chart = new Chart(document.getElementById('output_function'), output_function_config);
+    output_function_chart.data.labels = data.map(row => row.year);
+    output_function_chart.data.datasets[0].data = data.map(row => row.count);
+    output_function_chart.update();
+
+    data.forEach(n => {
+        bode_chart.data.labels.push(n + "");
+        bode_chart.data.datasets[0].data.push(n.year);
+        bode_chart.data.datasets[1].data.push(n.count);
+    });
+    bode_chart.update();
 }
